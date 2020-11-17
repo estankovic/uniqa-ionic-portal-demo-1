4301dcd6fff86abab1d2e4a06aa3b4fc9c2c02df
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2020-11-17 12:46:55 +0100

    Added static data

--- src/app/pages/contracts/contracts.page.ts
+++ src/app/pages/contracts/contracts.page.ts
@@ -3,6 +3,16 @@ import {Platform} from '@ionic/angular';
 import {Router} from '@angular/router';
 import {ITabSwitcher} from '../../shared/components/tab-switcher/tab-switcher.interface';
 
+interface Record {
+  title: string;
+  id: string;
+  items: {
+    title: string;
+    label: string;
+    details: boolean
+  }[];
+}
+
 @Component({
   selector: 'app-contracts',
   templateUrl: './contracts.page.html',
@@ -11,6 +21,20 @@ import {ITabSwitcher} from '../../shared/components/tab-switcher/tab-switcher.in
 export class ContractsPage implements OnInit {
 
   @ViewChild('panel') tabController: ITabSwitcher;
+
+
+  data: Record[] = [0, 1, 2, 3].map(v => ({
+    id: '01/004/413906',
+    title: 'Privatarzt',
+    items: !!(v % 3) ? [] : [
+      {
+        title: '26.02.202 | Max Musterman • Apotheke',
+        label: 'In Bearbeitung',
+        details: !!(v % 2)
+      }
+    ]
+  }));
+
 
   get isMobile() {
     return this.platform.is('mobileweb') || this.platform.is('mobile');
