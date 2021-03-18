e2884f5542209b0f156a7dbb05a283e6b174c460
Stefan Marcin <stefan.marcin.ext@uniqa.at>
2021-03-18 14:25:52 +0100

    Firebase inited...

--- src/app/poker-app/pages/session-list/session-list.page.ts
+++ src/app/poker-app/pages/session-list/session-list.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-session-list',
-  templateUrl: './session-list.page.html',
-  styleUrls: ['./session-list.page.scss'],
+    selector: 'app-session-list',
+    templateUrl: './session-list.page.html',
+    styleUrls: ['./session-list.page.scss'],
 })
 export class SessionListPage implements OnInit {
 
-  constructor() { }
+    constructor(private firestore: AngularFirestore) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.firestore.collection('users').add({
+            first: 'Ada',
+            last: 'Lovelace',
+            born: 1815
+        })
+            .then((docRef) => {
+                console.log('Document written with ID: ', docRef.id);
+            })
+            .catch((error) => {
+                console.error('Error adding document: ', error);
+            });
+    }
 
 }
