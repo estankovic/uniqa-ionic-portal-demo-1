cd064eb4be7b14ce7ed135dc6a591105a04fe567
Jakub Juhas <jakub.juhas.ext@uniqa.at>
2021-03-19 11:01:58 +0100

    Added shared module

--- src/app/poker-app/pages/sm-current-task/sm-current-task.module.ts
+++ src/app/poker-app/pages/sm-current-task/sm-current-task.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +15,7 @@ import {SharedModule} from '../../shared/shared.module';
     FormsModule,
     IonicModule,
     SmCurrentTaskPageRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   declarations: [SmCurrentTaskPage]
 })
