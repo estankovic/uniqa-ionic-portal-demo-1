94154cf64fbb787fda79ef42b15725eee6edef4d
Stefan Marcin <stefan.marcin.ext@uniqa.at>
2021-03-19 15:01:18 +0100

    Welcome page added.

--- src/app/poker-app/pages/welcome/welcome.page.ts
+++ src/app/poker-app/pages/welcome/welcome.page.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
-import {uniqaMailValidator} from "./validator";
+import {uniqaMailValidator} from './validator';
+import {FirebaseService} from '../../shared/services/firebase.service';
 
 @Component({
     selector: 'app-welcome',
@@ -12,22 +13,25 @@ export class WelcomePage implements OnInit {
 
     loginForm: FormGroup;
     router: Router;
+    firebaseService: FirebaseService;
 
-    constructor(private fb: FormBuilder, private rt: Router) {
+    constructor(private fb: FormBuilder, private rt: Router, private fbs: FirebaseService) {
         this.loginForm = fb.group({
             email: new FormControl('', uniqaMailValidator()),
             name: new FormControl('', Validators.required)
         });
         this.router = rt;
+        this.firebaseService = fbs;
     }
 
     ngOnInit() {
     }
 
     submit() {
-        console.log('name: ', this.loginForm.controls.name.value);
-        console.log('email: ', this.loginForm.controls.email.value);
-        //TODO: save into localStorage, call firebase
+        this.firebaseService.createNewUser({
+            email: this.loginForm.controls.email.value,
+            name: this.loginForm.controls.name.value
+        });
         this.router.navigate(['/poker-app']);
     }
 
