7d8e2e798534740e4f7a8ccf0442f9438dd749d5
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2021-03-22 09:34:18 +0100

    Added is login guard

--- src/app/poker-app/pages/welcome/welcome.page.ts
+++ src/app/poker-app/pages/welcome/welcome.page.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {uniqaMailValidator} from './validator';
 import {FirebaseService} from '../../shared/services/firebase.service';
 
@@ -15,7 +15,12 @@ export class WelcomePage implements OnInit {
     router: Router;
     firebaseService: FirebaseService;
 
-    constructor(private fb: FormBuilder, private rt: Router, private fbs: FirebaseService) {
+    constructor(
+      private fb: FormBuilder,
+      private rt: Router,
+      private fbs: FirebaseService,
+      private route: ActivatedRoute,
+    ) {
         this.loginForm = fb.group({
             email: new FormControl('', uniqaMailValidator()),
             name: new FormControl('', Validators.required)
@@ -27,12 +32,15 @@ export class WelcomePage implements OnInit {
     ngOnInit() {
     }
 
-    submit() {
-        this.firebaseService.createNewUser({
+    async submit() {
+        await this.firebaseService.createNewUser({
             email: this.loginForm.controls.email.value,
             name: this.loginForm.controls.name.value
         });
-        this.router.navigate(['/poker-app']);
+
+        const redirect = this.route.snapshot.queryParams.redirect;
+
+        this.router.navigate(redirect ? [decodeURIComponent(redirect)] : ['/poker-app']);
     }
 
 }
