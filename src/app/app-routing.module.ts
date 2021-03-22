0a50900ff9ec737bfc443420dc9c372141fdd40c
Eduard Stankovič <eduard.stankovic@gmail.com>
2021-03-22 11:22:43 +0100

    Revert "Connect poker to ordinarica"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,7 @@ const routes: Routes = [
   {
     path: 'health-detail/:id',
     loadChildren: () =>
-      import('./pages/health-insurance-detail/health-insurance-detail.module').then(m => m.HealthInsuranceDetailPageModule)
-  },
-  {
-    path: 'poker-app',
-    loadChildren: () =>
-      import('./poker-app/poker-app-routing.module').then(m => m.PokerAppRoutingModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
+        import('./pages/health-insurance-detail/health-insurance-detail.module').then(m => m.HealthInsuranceDetailPageModule)
   },
 ];
 @NgModule({
